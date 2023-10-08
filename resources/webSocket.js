9af067ec85731764c7ba0a76b5502b8a4b423e65
khpallon <karlhannespallon@gmail.com>
2023-10-08 20:40:29 +0000

    displays number when typing realtime

--- resources/webSocket.js
+++ resources/webSocket.js
@@ -7,6 +7,21 @@ socket.onopen = () => {
     console.log("We are live!")
 }
 
+socket.addEventListener("open", (event) => {
+    console.log("WebSocket connection established.");
+});
+
+
+function typer(){
+    var har
+    var msg = document.getElementById("message").value
+    var tag = document.createElement("p");
+    var text = document.createTextNode(msg);
+    har = tag.appendChild(text);
+    var element = document.getElementById("middle");
+    element.appendChild(tag);
+}
+
 socket.onclose = (event) => {
     console.log("WS CONNECTION STOPPED", event)
     
